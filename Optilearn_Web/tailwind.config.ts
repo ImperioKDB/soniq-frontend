e793ccfa9951e496c16d6c70936749f2cf2d2b96
ImperioKDB <kingbrite123@gmail.com>
2026-04-05 00:58:59 +0100

    Create tailwind.config.ts

---
+++ Optilearn_Web/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        neon: {
+          cyan: "#00f2ff",
+          purple: "#7000ff",
+          dark: "#0a0a14",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
